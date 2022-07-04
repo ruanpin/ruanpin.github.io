0a7a245572ac2700c79d0cefb95668ad9126aa12
vincent <oa51360@gmail.com>
2022-07-04 18:22:56 +0800

    added checking window.width to change .listRWD display attr

--- function/barListSwitch.js
+++ function/barListSwitch.js
@@ -2,17 +2,21 @@
     var barShow = true;
     let barsBtn = document.getElementById("bars")
     let xBtn = document.getElementById("x")
+    let listRWD = document.getElementById("listRWD")
 
     function changeBtnStatus(){
         if(barShow){
             barShow = false
             barsBtn.style.display = "none"
             xBtn.style.display = "block"
+            listRWD.style.display = "block"
         }else{
             barShow = true
             xBtn.style.display = "none"
             barsBtn.style.display = "block"
+            listRWD.style.display = "none"
         }
+        
     }
 
     barsBtn.addEventListener("click", changeBtnStatus)
